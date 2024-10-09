f7454d95c0254f961fefeecc4e79bd6318847948
n0sej0b <garrett.l.rogers@gmail.com>
2024-10-08 22:01:25 -0600

    New

---
+++ Forum.js
@@ -0,0 +1,13 @@
+const freelancers = [
+{name: "Mcloven", price: 30, occupation: "Writer"},
+{name: "Bobby", price: 50, occupation: "Teacher"},
+{name: "Carol", price: 70, occupation: "Programmer"},
+{name: "Denise", price: 100, occupation: "Plumber"},
+{name: "Kyle", price: 10, occupation: "Dog Walker"},
+{name: "Doug", price: 25, occupation: "Cashier"},
+{name: "Jessica", price: 68, occupation: "Accountant"},
+
+
+
+    
+];
